10fc976fc77f46c04f5f4ebb40dc51e40fc67f87
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2026-03-06 08:09:30 -0500

    primer bot telegram

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, request
+import telebot
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="URL_AQUI" + TOKEN)
+    return "Bot funcionando", 200
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hola! Soy tu primer bot en Python")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Escribiste: " + message.text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
